554c567147b446a1bf59f8bb8486e1a2d8c1b68b
shupitsu <shupitsu@outlook.com>
2025-04-21 15:25:56 +0200

    update to v2

---
+++ Generators/GeneratorGlobalStrings.py
@@ -0,0 +1,2 @@
+versionString = '<!-- __GAMEVERSION__ -->\n'
+botInfoString = '<!-- This section has been added automatically by a bot. Please contact [[User:Alleryn]] or [[user:Shupitsu]] who keeps the bot up to date (check page history for link to user page with contact info) if you believe this section is in error. -->\n'
